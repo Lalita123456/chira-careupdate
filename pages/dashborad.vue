3a09dfa776924536b347b45df5c900562bece3d7
Lalita123456 <6431503051@lamduan.mfu.ac.th>
2024-03-30 00:11:28 +0700

    initial file

---
+++ pages/dashborad.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="bg-red-900 ...">
+            Save changes
+        </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button class="bg-indigo-500 ...">
+            Save changes
+        </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
