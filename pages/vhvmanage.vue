143856c356f7992a03fe7cdb178ce5dbea575907
Lalita Ruttanarungsri <6431503051@lamduan.mfu.ac.th>
2024-06-17 20:33:04 +0700

    update login page & vhvmanage page

--- pages/vhvmanage.vue
+++ pages/vhvmanage.vue
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <div class="page">
         <div class="sidebar">
-            <img src="/assets/pictures/logotab.png" alt="logotab"/>
+            <img src="/assets/pictures/logotab.png" alt="logotab" />
 
             <div class="sidebar-item">
                 <div class="sidebar-icon">
@@ -51,21 +51,50 @@
 
             <div class="hr"></div>
         </div>
-
         <div class="content">
-            
+            <!-- Top Bar -->
+            <div class="flex items-center justify-between">
+                <div class="top-bar-header">
+                    <h1>จัดการข้อมูลเจ้าหน้าที่ติดตามผู้ป่วย</h1>
+                </div>
+                <!-- Nav bar -->
+                <div style="display: flex; align-items: center; margin: 0 10px">
+                    <i class="fa-regular fa-bell" style="color: black; margin: 0 8px; padding: 8px; border-radius: 5px; background-color: #e5e7eb"></i>
+                    <img style="width: 35px; height: 35px; margin: 0 5px; border-radius: 10px;" src="https://scontent.fcnx4-1.fna.fbcdn.net/v/t39.30808-6/340812573_1222119048693459_7878843100786500839_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=b9yduR4LQ9kQ7kNvgGaKCrb&_nc_ht=scontent.fcnx4-1.fna&oh=00_AYDRLmFq01Xm3Kk-IHJEmezeU7fczDbwG1Czn6OgCs2UYA&oe=667260C2" alt="">
+                    <p style="font-size: 10px; font-weight: 500;">Kanlayanee</p>
+                </div>
+            </div>
+
         </div>
+       
     </div>
+
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@500&family=Mali:wght@700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
 
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: "Noto Sans", sans-serif;
+}
+
+/* .top-bar {
+    background-color: #5D97EF;
+    display: flex;
+    align-items: center;
+} */
+
+
+h1 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    font-style: normal;
+    font-size: 30px;
+    padding: 10px;
 }
 
 .page {
@@ -77,22 +106,16 @@
 }
 
 .sidebar {
-    width: 20%;
-    padding: 25px;
-}
-
-h3 {
-    font-size: 1.5rem;
+    width: 25%;
+    padding: 15px;
 }
 
 .content {
     width: 100%;
-    background-color: rgb(86, 124, 228);
+    background-color: rgb(255, 255, 255);
+    /* border: 5px solid black; */
 }
 
-/* .sidebar-main {
-    margin-top: 30px;
-} */
 
 .sidebar-item {
     display: flex;
